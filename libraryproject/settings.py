7fa67ec27fc0dbca3e3488d63f7e4df4d5e2045c
Maldughaim <mohaldughaim@gmail.com>
2024-10-30 21:28:54 +0300

    lab7

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,8 +54,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'libraryproject.urls'
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR, "apps" + os.sep + "templates")
+TEMPLATE_DIR = os.path.join(BASE_DIR, "apps/templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -121,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, "apps/static"))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
